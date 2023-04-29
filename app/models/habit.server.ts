503c906e3899ee11a0d281a3c3564dcfa1517f50
shivan-vickers <shivan.vickers@gmail.com>
2023-04-28 21:49:01 -0700

    Add CRUD functions for habit model

--- app/models/habit.server.ts
+++ app/models/habit.server.ts
@@ -3,14 +3,28 @@ import { prisma } from "~/utils/prisma.server";
 
 export type { Habit } from "@prisma/client";
 
-export async function createHabit({ groupId, content }: Habit) {
+export async function createHabit(
+  groupId: Habit["groupId"],
+  content: Habit["content"]
+) {
   return prisma.habit.create({ data: { groupId, content } });
+}
+
+export async function getHabitById(id: Habit["id"]) {
+  return prisma.habit.findUnique({ where: { id } });
 }
 
 export async function getHabitsByGroupId(groupId: HabitGroup["id"]) {
   return prisma.habit.findMany({ where: { groupId } });
 }
 
-export async function updateHabitById({ groupId, content }: Habit) {}
+export async function updateHabitById(
+  id: Habit["id"],
+  content: Habit["content"]
+) {
+  return prisma.habit.update({ where: { id }, data: { content } });
+}
 
-export async function deleteHabitById(id: Habit["id"]) {}
+export async function deleteHabitById(id: Habit["id"]) {
+  return prisma.habit.delete({ where: { id } });
+}
