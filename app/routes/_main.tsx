41733ab9765b65a54096b2fda13d6cfa3b552274
shivan-vickers <shivan.vickers@gmail.com>
2023-04-30 18:18:21 -0700

    Add ErrorContainer to main layout

--- app/routes/_main.tsx
+++ app/routes/_main.tsx
@@ -5,7 +5,8 @@ import {
   useRouteError,
 } from "@remix-run/react";
 
-import { Sidebar } from "~/components/Sidebar";
+import type { ErrorMessage } from "~/components/ErrorContainer";
+import { ErrorContainer } from "~/components/ErrorContainer";
 
 export default function Main() {
   return (
@@ -21,25 +22,20 @@ export default function Main() {
 }
 
 export function ErrorBoundary() {
-  let error = useRouteError;
+  const error = useRouteError();
 
-  function errorContainer(heading?: any, content?: any) {
-    return (
-      <div className="m-12 border border-redDark-red6 bg-redDark-red2 p-4 text-redDark-red9">
-        {heading ? <h1 className="text-lg font-semibold">{heading}</h1> : null}
-        {content ? <pre>{content}</pre> : null}
-      </div>
-    );
-  }
+  let { heading, content }: ErrorMessage = {
+    heading: "Unknown Error",
+    content: "I actually have no idea what happened here. Sorry!",
+  };
 
   if (isRouteErrorResponse(error)) {
-    return errorContainer(`${error.status} ${error.statusText}`, error.data);
+    heading = ` ${error.status} ${error.statusText}`;
+    content = error.data;
   } else if (error instanceof Error) {
-    return errorContainer(error.message, error.stack);
-  } else {
-    return errorContainer(
-      "Unknown Error",
-      "I actually have no idea what happened here. Sorry!"
-    );
+    heading = error.message;
+    content = error.stack;
   }
+
+  return <ErrorContainer heading={heading} content={content} />;
 }
